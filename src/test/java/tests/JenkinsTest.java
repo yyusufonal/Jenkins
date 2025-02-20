f0cbd0096b02a4942eefa5db6297916a37ad99a9
unknown <yusufonal03@gmail.com>
2025-02-20 22:31:46 +0300

    yusuf

---
+++ src/test/java/tests/JenkinsTest.java
@@ -0,0 +1,48 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class JenkinsTest {
+
+	@Test
+	void Test1(){
+		assertTrue(true);
+	}
+	@Test
+	void Test2(){
+		assertTrue(true);
+	}
+	@Test
+	void Test3(){
+		assertTrue(true);
+	}
+	@Test
+	void Test4(){
+		assertTrue(true);
+	}
+	@Test
+	void Test5(){
+		assertTrue(true);
+	}
+	@Test
+	void Test6(){
+		assertTrue(true);
+	}
+	@Test
+	void Test7(){ assertTrue(true);}
+	@Test
+	void Test8(){
+		assertTrue(true);
+	}
+	@Test
+	void Test9(){
+		assertTrue(true);
+	}
+	@Test
+	void Test10(){
+		assertTrue(true);
+	}
+
+}
